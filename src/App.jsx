a70ba7712869bc0e3c4d9702e92159446c0c575f
m2lovric <m2lovric@gmail.com>
2021-09-16 13:48:55 +0200

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <header>
+        <h3>_MATTEO</h3>
+      </header>
+      <main>
+        <h1>
+          Hi there, I’m Matteo.
+          <br />
+          Front End Developer,
+          <br />
+          and CNC Operator.
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
